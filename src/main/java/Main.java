0316088b84e3c4bc84e4e0b54d8bdadf5ee1f7ee
Noah Leibovitz <noah.leibovitz@outlook.com>
2026-07-06 09:53:25 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.zip.GZIPInputStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -9,9 +8,17 @@ public class Main {
             String ans = sc.nextLine();
             if (ans.equals("exit") || ans.startsWith("exit ")) {
                 break;
-            }
+            } if(ans.startsWith("echo ")){
 
-            if(ans.equals("echo") || ans.startsWith("echo ")){
+                switch(ans.substring(4)){
+                    case "echo", "exit", "type" -> {
+                        System.out.println(ans.substring(4) + "is a shell builtin");
+                    }
+                    default -> { System.out.println(ans.substring(4) + ": not found"); }
+                    
+                }
+            }
+            else if(ans.equals(" echo") || ans.startsWith("echo ")){
                 System.out.println(ans.length() > 4 ? ans.substring(5) : "");
             } else {
                 System.out.println(ans + ": command not found");
