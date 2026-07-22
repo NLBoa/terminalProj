9f8b7f226c185ea5535ac4e4b0df7f46aec8b42f
Noah Leibovitz <noah.leibovitz@outlook.com>
2026-07-22 15:29:08 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -92,7 +92,7 @@ public class Main {
 
         for(int i = 0; i < input.length(); i++){
             char c = input.charAt(i);
-            if(c == '\\' && backslashStarted == false)
+            if(c == '\\' && backslashStarted == false && inSingleQuotes == false)
             { 
                 backslashStarted = true;
                 tokenStarted = true;
