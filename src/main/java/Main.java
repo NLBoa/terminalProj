2c504d101d141717433bb3a234d0b3ceeaeac37e
Noah Leibovitz <noah.leibovitz@outlook.com>
2026-07-22 13:32:08 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -92,7 +92,7 @@ public class Main {
 
         for(int i = 0; i < input.length(); i++){
             char c = input.charAt(i);
-            if(c == '\\')
+            if(c == '\\' && backslashStarted == false)
             { 
                 backslashStarted = true;
                 tokenStarted = true;
